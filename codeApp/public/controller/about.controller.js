b485d5345f4e0f468b37bf5147d6d145512d010a
rohan2jos <rohan2jos@gmail.com>
2016-06-29 10:17:42 -0500

    seperated the controllers into different files

--- codeApp/public/controller/about.controller.js
+++ codeApp/public/controller/about.controller.js
@@ -3,7 +3,9 @@
     06/28/2016
 */
 
-app.controller('aboutController', function(){
+// this file will create the aboutController that will be referenced to by the about.html
+
+angular.module("codeApp").controller('aboutController', function($scope){
     console.log("welcome to the about controller");
     $scope.title = "tempAbout";
 });
--- codeApp/public/controller/home.controller.js
+++ codeApp/public/controller/home.controller.js
@@ -3,11 +3,16 @@
     06/28/2016
 */
 
-app.controller('homeController', function(){
+// we will now create the controller : homeController.
+// The best practice is to put the controller in an IIFE, but for the sake of understandability and
+// readability, we will use the plain javascript method
+
+angular.module("codeApp").controller('homeController', function(){
     console.log("welcome to the home controller, you have now wired up the app successfully, start coding!");
 });
 
+/*
 app.controller('aboutController', function($scope){
     console.log("welcome to the about controller");
     $scope.title = "tempAbout";
-});
+});*/
