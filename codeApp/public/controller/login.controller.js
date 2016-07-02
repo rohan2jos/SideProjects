d3ebb3bb66a16f39cff61a90a7b5aecc269802a5
rohan2j <rohan2j@ccs.neu.edu>
2016-07-02 11:39:58 -0500

    now redirects to home on successful login

--- codeApp/public/controller/login.controller.js
+++ codeApp/public/controller/login.controller.js
@@ -1,4 +1,4 @@
-angular.module("codeApp").controller("loginController", function($scope, $rootScope){
+angular.module("codeApp").controller("loginController", function($scope, $rootScope, $location){
 
 	$scope.login = login;	
 
@@ -22,6 +22,7 @@ angular.module("codeApp").controller("loginController", function($scope, $rootSc
 				console.log("Welcome, " + $scope.user.username);
                 $rootScope.currentUser = $scope.users[i];
                 console.log("login controller sets the rootscope user as " + $rootScope.currentUser.username);
+                $location.url("/home");
 				break;
 			}
 		}
