af70e50bb35696bc2d666d065df84c8e9a808b3e
rohan2jos <rohan2jos@gmail.com>
2016-07-01 21:00:34 -0500

    added login controller array

--- codeApp/public/controller/login.controller.js
+++ codeApp/public/controller/login.controller.js
@@ -9,6 +9,21 @@ angular.module("codeApp").controller("loginController", function($scope){
 	function login(){
 		console.log("you just clicked on the login function");
 		console.log("the username is " + $scope.user.username + " and the password is " + $scope.user.password);
+
+
+		
+		$scope.users = [{username: "rohan", password: "rohan"}, {username: "bob", password: "bob"}, {username: "fred", password: "fred"}];
+
+
+		console.log("validating ...");
+		for(i = 0; i<$scope.users.length; i++){
+			console.log("checking for " + $scope.users[i].username);
+			if($scope.users[i].username == $scope.user.username && $scope.users[i].password == $scope.user.password){
+				console.log("Welcome, " + $scope.user.username);
+				break;
+			}
+		}
+
 	}
 
 })
