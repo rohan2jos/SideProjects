afba5fab62b77dee4a835bbe965a0f07c4b24ba4
rohan2j <rohan2j@ccs.neu.edu>
2016-07-02 22:38:17 -0500

    added register page and blank register controller; added dependency for angular-animate; views now have a small animation;

---
+++ codeApp/public/controller/register.controller.js
@@ -0,0 +1,13 @@
+angular.module("codeApp").controller("registerController", function($scope, $rootScope, UserService){
+    console.log("register controller is locked and loaded.");
+    
+    $scope.register = register;
+    
+    function register(){
+        console.log("trying to register user");
+        
+        
+    }
+    
+    
+});
