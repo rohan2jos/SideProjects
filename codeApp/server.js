c3b8e7e6fa76cd7f0c2b4dd62f4142b04bf5faf7
rohan2j <rohan2j@ccs.neu.edu>
2016-07-04 11:24:58 -0500

    added server side empty files; begin wiring server to client; added toasts on login and logout

--- codeApp/server.js
+++ codeApp/server.js
@@ -5,17 +5,24 @@
 
 var express = require('express');
 var app = express();
+var https = require('https');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var uuid = require('node-uuid');
 var cookieParser  = require('cookie-parser');
 
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use(express.static(__dirname + '/public'));
+
+require('./public/server/app.js')(app, uuid);
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, ipaddress);
 
