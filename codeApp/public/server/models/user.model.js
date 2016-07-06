fb983836673432442ebeee47296a142c1f6c4abd
rohan2j <rohan2j@ccs.neu.edu>
2016-07-05 23:16:55 -0500

    backend wired up, and api endpoint for login works

--- codeApp/public/server/models/user.model.js
+++ codeApp/public/server/models/user.model.js
@@ -1,3 +1,41 @@
+// requiring the users file
+// this will later go into the mongoose
+// for now, we shall keep the users as a json file
+var users = require("./user.mock.json");
+
 module.exports = function(){
+    
+    // this will later go into the api endpoints
+    // for now, we will just assign functions as values
+    // to the keys
+    var api = {
+        findUserByCredentials: findUserByCredentials,
+        createUser: createUser
+    };
+    return api;
+    
+    
+    // when a new user signs up
+    function createUser(user){
+        // user is the json object that we will receive with username and password and email
+        users.push(user);
+    }
+    
+    // when a user is trying to login
+    function findUserByCredentials(credentials){
+        var username = credentials.username;
+        var password = credentials.password;
+        var userPresent = null;
+        for(var u in users){
+            if(users[u].username === username && users[u].password === password){
+                console.log("user " + username + " has logged in successfully");
+                userPresent = users[u];
+                break;
+            }
+        }
+        
+        return userPresent;
+    }
+    
     
 };
--- codeApp/public/server/services/user.service.server.js
+++ codeApp/public/server/services/user.service.server.js
@@ -1,3 +1,26 @@
 module.exports = function(app, userModel){
+
+    // this is the api endpoints
+    // the http requests are caught here
+    app.post("/api/codeApp/user", createUser);
+    app.post("/api/codeApp/login", loginWithCredentials);
     
+    
+    function createUser(req, res){
+        console.log("[createUser] hit the backend, first checkpoint reached");
+        var user = req.body;
+        console.log("The user that is requesting to be inserted is " + user);
+        var userCreated = userModel.createUser(user);
+        res.json(userCreated);
+    }
+    
+    
+    
+    function loginWithCredentials(req, res) {
+        var credentials = req.body;
+        console.log("in server");
+        console.log(credentials.username);
+        var currentUser = userModel.findUserByCredentials(credentials);
+        res.json(currentUser);
+    }
 }
