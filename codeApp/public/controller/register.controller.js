7f4ab53c24d7215d6d48bf0e01b60316dcc60cd4
rohan2jos <rohan2jos@gmail.com>
2016-07-06 11:38:53 -0500

    can now register without mongo, next step is mongo

--- codeApp/public/controller/register.controller.js
+++ codeApp/public/controller/register.controller.js
@@ -1,12 +1,13 @@
-angular.module("codeApp").controller("registerController", function($scope, $rootScope, UserService){
+angular.module("codeApp").controller("registerController", function($scope, $rootScope, UserService, $location){
     console.log("register controller is locked and loaded.");
     
     $scope.register = register;
     
     function register(){
-        console.log("trying to register user");
-        
-        
+        console.log("trying to register user " + $scope.user);
+        // we will send the user as is in the json format to the user service    
+        UserService.createUser($scope.user);
+        $location.url("/home");
     }
     
     
